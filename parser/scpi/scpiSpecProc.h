b6195fcb121644f94817272807903ab3174b725c
yaochein <yaochien1127@gmail.com>
2018-06-01 14:49:44 +0800

    Delete scpiSpecProc.h

--- parser/scpi/scpiSpecProc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SCPI_SPECIAL_PROCESS_H__
-#define __SCPI_SPECIAL_PROCESS_H__
-
-#include "scpiStruct.h"
-
-extern int scpiSpecialProcess(STR_PARSER *p);
-
-#endif //__SCPI_SPECIAL_PROCESS_H__
-
