f8bd10bb1232be50e582525474df98d8b6bc5ecf
yaochein <yaochien1127@gmail.com>
2018-06-01 13:55:39 +0800

    Delete Keypad.h

--- Keypad.h
+++
@@ -1,9 +0,0 @@
-extern void BackSpaceFunc(void);
-extern void ShowKeyPadString(char* STR);
-extern void KeypadUpdate(void);
-extern void ShowString(char* STR,int x,int y,int pos);
-extern void KeySelFunc(BYTE cnt);
-extern void DrawKeySelPtr(void);
-extern void ClrKeySelPtr(void);
-extern void EnterChrFunc(void);
-extern void KeyPadProc(void);
